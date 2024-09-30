d1e781dd76712b6eacd85734c5fb76d53fdfa254
Uladzislau Tsemenka <wladyslaw.tsemenko@gmail.com>
2024-09-30 20:56:05 +0300

    second

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
