270bc97fef570454443023f06befe04f9e3ac6c9
Uladzislau Tsemenka <wladyslaw.tsemenko@gmail.com>
2024-09-30 21:32:01 +0300

    third

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const { sql } = require("@vercel/postgres");
 
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.post("/post", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post("/post", async (req, res) => {
+  try {
+    await sql`INSERT INTO data (data) VALUES (${req.body});`;
+    res.status(200).send("Success");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
